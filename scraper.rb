5e64b2a7da2ec916915a34acb5b26090e9ea6788
Benjamin Harris <Bigben83@users.noreply.github.com>
2025-02-26 12:04:19 +1100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -70,7 +70,13 @@ doc.css('.edn_article').each_with_index do |item, index|
   applicant = item.at_css('.edn_articleTitle.edn_articleSubTitle') ? item.at_css('.edn_articleTitle.edn_articleSubTitle').text.sub('APPLICANT:', '').strip : 'NA'
 
   # Extract the description (from the article summary)
+  # description = item.at_css('.edn_articleSummary') ? item.at_css('.edn_articleSummary').text.strip.sub('PROPOSAL:', '').strip : 'NA'
+  # Extract the description (from the article summary)
   description = item.at_css('.edn_articleSummary') ? item.at_css('.edn_articleSummary').text.strip.sub('PROPOSAL:', '').strip : 'NA'
+
+  # Clean up the description to remove unnecessary metadata
+  description_cleaned = description.split("LOCATION:").first.strip
+  description_cleaned = description_cleaned.split("CLOSES:").first.strip
 
   # Extract the location (from the article summary)
   address = item.at_css('.edn_articleSummary') ? item.at_css('.edn_articleSummary').text.split('LOCATION:').last.split('CLOSES:').first.strip : 'NA'
